9511dbb5247bf4a83c12ac2a0fb9bf61ca4b522f
wobsoriano <sorianorobertc@gmail.com>
2023-07-01 10:03:50 -0700

    update test

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm --filter solid-sonner build && pnpm dev',
+    command: 'pnpm dev',
     url: 'http://127.0.0.1:3000',
     cwd: './test',
     reuseExistingServer: !process.env.CI,
