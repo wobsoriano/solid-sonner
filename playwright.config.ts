3224574932005893d61492fb5b1f5c544e3ec6ed
wobsoriano <sorianorobertc@gmail.com>
2024-02-09 13:38:28 -0800

    fix incorrect test port

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run dev',
-    port: 4173,
+    port: 3000,
   },
   testDir: 'test',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
